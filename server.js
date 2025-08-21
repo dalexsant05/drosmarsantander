abd4e5fda5cc28ae660e379add3d2ee7c2a9b0eb
Daniel Santander <desarrollosalud3@sio.com.co>
2025-08-20 23:58:22 -0500

    Pruebas

--- server.js
+++ server.js
@@ -1,35 +1,46 @@
-const express = require('express');
-const path = require('path');
-const livereload = require('livereload');
-const connectLivereload = require('connect-livereload');
+// const express = require('express');
+// const path = require('path');
+// const livereload = require('livereload');
+// const connectLivereload = require('connect-livereload');
 
+// const app = express();
+
+// // 👇 Livereload server para la carpeta "public" y "views"
+// const liveReloadServer = livereload.createServer();
+// liveReloadServer.watch([
+//   path.join(__dirname, "public"),
+//   path.join(__dirname, "views")
+// ]);
+
+// // 👇 Middleware para inyectar el script de livereload
+// app.use(connectLivereload());
+
+// // Actualiza el navegador cuando detecta cambios
+// liveReloadServer.server.once("connection", () => {
+//   setTimeout(() => {
+//     liveReloadServer.refresh("/");
+//   }, 100);
+// });
+
+// // Configuración del servidor Express
+// app.set('view engine', 'ejs');
+// app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
+
+// app.listen(3000, () => {
+//   console.log('Servidor en http://localhost:3000');
+// });
+const express = require('express');
 const app = express();
 
-// 👇 Livereload server para la carpeta "public" y "views"
-const liveReloadServer = livereload.createServer();
-liveReloadServer.watch([
-  path.join(__dirname, "public"),
-  path.join(__dirname, "views")
-]);
-
-// 👇 Middleware para inyectar el script de livereload
-app.use(connectLivereload());
-
-// Actualiza el navegador cuando detecta cambios
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
-});
-
-// Configuración del servidor Express
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-
 app.get('/', (req, res) => {
-  res.render('index');
+  res.send('<h1>¡Hola! El servidor está funcionando correctamente.</h1>');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`);
 });
